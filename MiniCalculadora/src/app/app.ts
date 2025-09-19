dc21d33c3254af3d29910798b3d69c1977a5165b
CarlosMEsteban <estgarca@gmail.com>
2025-09-19 13:21:21 +0200

    Funciona bidirecciopanla

--- MiniCalculadora/src/app/app.ts
+++ MiniCalculadora/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'MiniCalculadora';
+  protected title = 'Mini calculadora';
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  sumar() {
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
